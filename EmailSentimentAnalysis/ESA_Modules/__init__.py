af0a2ffb6f3c11d5a848a150c70d16b970bff839
Dan <147379933+Dan-QWDXY006@users.noreply.github.com>
2023-10-22 11:31:12 +1100

    removed interface due to the use of dash requring access throughout the entire program and can not be encapsulated.

--- EmailSentimentAnalysis/ESA_Modules/__init__.py
+++ EmailSentimentAnalysis/ESA_Modules/__init__.py
@@ -1,7 +1,6 @@
 # Handles ESA Module Importing as a Package
 from .ESA_DatasetBuilder import *
 from .ESA_Exporter import *
-from .ESA_Interface import *
 from .ESA_PreprocessedEmailAccess import *
 from .ESA_ProcessedEmailAccess import *
 from .ESA_RawEmailAccess import *
--- EmailSentimentAnalysis/app.py
+++ EmailSentimentAnalysis/app.py
@@ -1,10 +1,6 @@
 # This is the main python file to be executed
-from ESA_Modules import Interface
 import webbrowser
-
-program_interface = Interface("Sentiment Analysis")
 
 webbrowser.get().open("http://127.0.0.1:8050")
 
-program_interface.run()
 
